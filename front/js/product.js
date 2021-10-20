6626a071e00a29f6f26135b280911c35862ee214
augt <augu.tacquet@gmail.com>
2021-10-20 19:40:43 +0200

    renamed product variable to productIndex

--- front/js/product.js
+++ front/js/product.js
@@ -80,18 +80,18 @@ addToCartButton.addEventListener('click', function(){
 
       arrayProductsInCart = JSON.parse(localStorage.getItem("products"));
 
-      const product = arrayProductsInCart.findIndex(
+      const productIndex = arrayProductsInCart.findIndex(
         (product) =>
           product.id === productAdded.id && product.color === productAdded.color
       );
 
-      if (product === -1) { // if an identical item isn't already present, then we add the new item to the array
+      if (productIndex === -1) { // if an identical item isn't already present, then we add the new item to the array
  
         arrayProductsInCart.push(productAdded);
         localStorage.setItem("products", JSON.stringify(arrayProductsInCart));
 
       } else { // if an identical item is already present in the local storage, then we increment the quantity of said item
-        arrayProductsInCart[product].quantity = parseInt(arrayProductsInCart[product].quantity) + parseInt(productAdded.quantity);
+        arrayProductsInCart[productIndex].quantity = parseInt(arrayProductsInCart[productIndex].quantity) + parseInt(productAdded.quantity);
         localStorage.setItem("products", JSON.stringify(arrayProductsInCart));
 
 
