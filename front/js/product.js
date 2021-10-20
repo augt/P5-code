eb5cb3d10e49c0e930d5cbbcf66592b5bc893815
augt <augu.tacquet@gmail.com>
2021-10-20 15:48:52 +0200

    add to cart working without increment function

--- front/js/product.js
+++ front/js/product.js
@@ -4,45 +4,99 @@ let id = params.get("id");
 let protocol ="http";
 let domain = "localhost:3000";
 
+const productName = document.querySelector("#title");
+const price = document.querySelector("#price");
+const colorsMenu = document.querySelector("#colors");
+const quantity = document.querySelector("#quantity");
+const addToCartButton = document.querySelector("#addToCart")
+
+// retrieve product informations and display it on the page
+
 fetch( protocol + "://" + domain + "/api/products/" + id)
-    .catch ((error) => {
-      let container = document.querySelector(".item");
-      container.innerHTML = "<article>Nous n'avons pas réussi à afficher les articles.<br> <br>Avez-vous bien lancé le serveur local (Port 3000) ? <br><br>Si le problème persiste, contactez-nous.</article>";
+  .catch ((error) => {
+    let container = document.querySelector(".item");
+    container.innerHTML = "<article>Nous n'avons pas réussi à afficher les articles.<br> <br>Avez-vous bien lancé le serveur local (Port 3000) ? <br><br>Si le problème persiste, contactez-nous.</article>";
     })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function(apiResults) {
-        const sofa = apiResults;
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function(apiResults) {
+    const sofa = apiResults;
 
-        document.title = sofa.name;
+    document.title = sofa.name;
 
-        let productPicture = document.createElement("img");
-        document.querySelector(".item__img").appendChild(productPicture);
-        productPicture.setAttribute("src", sofa.imageUrl);
+    let productPicture = document.createElement("img");
+    document.querySelector(".item__img").appendChild(productPicture);
+    productPicture.setAttribute("src", sofa.imageUrl);
 
-        let productName = document.querySelector("#title");
-        productName.innerText = sofa.name;
+    productName.innerText = sofa.name;
 
-        let price = document.querySelector("#price");
-        price.innerText = sofa.price;
+    price.innerText = sofa.price;
 
-        let productDescription = document.querySelector("#description");
-        productDescription.innerText = sofa.description;
+    let productDescription = document.querySelector("#description");
+    productDescription.innerText = sofa.description;
 
-        let colors= sofa.colors;
-        for (let color of colors){
+    let colors= sofa.colors;
+    for (let color of colors){
 
-          let colorsMenu = document.querySelector("#colors");
-          let colorOption = document.createElement("option");
-          colorsMenu.appendChild(colorOption);
-          colorOption.setAttribute("value", color);
-          colorOption.innerText = color;
+          
+      let colorOption = document.createElement("option");
+      colorsMenu.appendChild(colorOption);
+      colorOption.setAttribute("value", color);
+      colorOption.innerText = color;
+    };
+});
 
-        };
+
+// adding items to local storage
+
+
+
+
+addToCartButton.addEventListener('click', function(){
+
+  if (quantity.value > 0 && colorsMenu.value !== "") {
+
+    let arrayProductsInCart = [];
+
+    let productAdded = {
+        id: id,
+        quantity: quantity.value,
+        color: colorsMenu.value,
 
     }
-    )
-    ;
+    
+
+    if (localStorage.getItem("products") == null){
+
+      arrayProductsInCart.push(productAdded);
+      localStorage.setItem("products", JSON.stringify(arrayProductsInCart));
+
+    } else {
+      
+      arrayProductsInCart = JSON.parse(localStorage.getItem("products")); 
+      arrayProductsInCart.push(productAdded);
+      localStorage.setItem("products", JSON.stringify(arrayProductsInCart));
+    };
+    
+    
+    console.log(arrayProductsInCart);
+    console.log(localStorage);
+
+  }
+
+  
+
+})
+
+
+
+
+
+
+
+
+
+
 
     
