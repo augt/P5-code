6fa6e88135876e878405721f1241d941d03d2dad
augt <augu.tacquet@gmail.com>
2021-10-23 17:16:07 +0200

    Implemented delete button function

--- front/js/cart.js
+++ front/js/cart.js
@@ -7,7 +7,7 @@ let arrayItemsPrices =[];
 
 for (item of arrayProductsInCart){
 
-    let itemTotalPrice = item.price*item.quantity
+    let itemTotalPrice = item.price*item.quantity;
 
     // display of cart products
     const cartContent = `<article class="cart__item" data-id="${item.id}">
@@ -35,7 +35,7 @@ for (item of arrayProductsInCart){
         .getElementById('cart__items')
         .insertAdjacentHTML('beforeend', cartContent);
  
-    //calculate total quantity of items and total price of cart
+    //constitue arrays to calculate quantity of items and total price of cart
 
     arrayQuantities.push(item.quantity);
 
@@ -43,22 +43,60 @@ for (item of arrayProductsInCart){
 
 }
 
+//calculate total quantity of items and total price of cart
+let reducer = (previousValue, currentValue) => previousValue + currentValue;
 
-console.log(arrayQuantities);
+function displayCartBalance() {
+    if (arrayQuantities.length !== 0) {    
 
-const reducer = (previousValue, currentValue) => previousValue + currentValue;
+        let totalQuantityInCart = arrayQuantities.reduce(reducer);
 
-let totalQuantityInCart = arrayQuantities.reduce(reducer);
+        document.querySelector("#totalQuantity").innerHTML = totalQuantityInCart;
 
-document.querySelector("#totalQuantity").innerHTML = totalQuantityInCart;
+        let totalPriceOfCart = arrayItemsPrices.reduce(reducer);
 
-let totalPriceOfCart = arrayItemsPrices.reduce(reducer);
+        document.querySelector("#totalPrice").innerHTML = totalPriceOfCart;
 
-document.querySelector("#totalPrice").innerHTML = totalPriceOfCart;
+    } else {
+        document.querySelector("#totalQuantity").innerHTML = 0;
 
-console.log(totalPriceOfCart)
+        document.querySelector("#totalPrice").innerHTML = 0;
+    };
+};
+displayCartBalance();
 
 
+// implementing delete button function
+
+let deleteButtonsList = document.querySelectorAll('.deleteItem');
+
+for (let deleteButton of deleteButtonsList) {
+
+    deleteButton.addEventListener('click', function(){
+
+        let parentArticleTag = deleteButton.closest("article");
+        let dataID = parentArticleTag.getAttribute("data-id");
+
+        let colorTagParent = deleteButton.closest("article > div");
+        let colorTag = colorTagParent.getElementsByTagName("p")[0];
+
+        let productIndex = arrayProductsInCart.findIndex(
+            (product) =>
+              product.id === dataID && product.color === colorTag.innerHTML
+        );
+
+        arrayProductsInCart.pop(productIndex);
+
+        localStorage.setItem("products", JSON.stringify(arrayProductsInCart));
+
+        parentArticleTag.remove();
+
+        arrayQuantities.pop(productIndex);
+        arrayItemsPrices.pop(productIndex);
+
+        displayCartBalance();
+    })
+}
 
 
 
