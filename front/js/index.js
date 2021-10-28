ff2a931287a3e20492b292a5b37c7397d0bee26b
augt <augu.tacquet@gmail.com>
2021-10-28 12:25:37 +0200

    Commenting index js file

--- front/js/index.js
+++ front/js/index.js
@@ -1,43 +1,56 @@
 let protocol ="http";
 let domain = "localhost:3000"
 
+displayAllProducts();
 
-fetch( protocol + "://" + domain + "/api/products/")
-    .then(function(res) {
-      if (res.ok) {
-        return res.json();
-      }
-    })
-    .then(function(apiResults) {
-      const sofas = apiResults;
-      for (let sofa of sofas) {
-        let productCard = document.createElement("a");
-        document.querySelector("#items").appendChild(productCard);
-        productCard.setAttribute("href", "product.html?id=" + sofa._id);
+function displayAllProducts() {
 
-        let productArticle = document.createElement("article");
-        productCard.appendChild(productArticle);
+  // retrieve products data from the API
 
-        let productPicture = document.createElement("img");
-        productArticle.appendChild(productPicture);
-        productPicture.setAttribute("src", sofa.imageUrl);
-        productPicture.setAttribute("alt", sofa.altTxt);
+  fetch( protocol + "://" + domain + "/api/products/")
+      .then(function(res) {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then(function(apiResults) {
+        const sofas = apiResults;
 
-        let productTitle = document.createElement("h3");
-        productArticle.appendChild(productTitle);
-        productTitle.classList.add("productName");
-        productTitle.innerText = sofa.name;
+        // display each product
+        layoutDataInDOM();
+        
+        function layoutDataInDOM(){
+          for (let sofa of sofas) {
+            let productCard = document.createElement("a");
+            document.querySelector("#items").appendChild(productCard);
+            productCard.setAttribute("href", "product.html?id=" + sofa._id);
 
-        let productDescription= document.createElement("p");
-        productArticle.appendChild(productDescription);
-        productDescription.classList.add("productDescription");
-        productDescription.innerText = sofa.description ;
+            let productArticle = document.createElement("article");
+            productCard.appendChild(productArticle);
 
-      }
-    })
-    .catch(function(err) {
-        document
-        .getElementById("items")
-        .innerText = "Nous n'avons pas réussi à afficher les articles. Avez-vous bien lancé le serveur local (Port 3000) ?";
-    });
+            let productPicture = document.createElement("img");
+            productArticle.appendChild(productPicture);
+            productPicture.setAttribute("src", sofa.imageUrl);
+            productPicture.setAttribute("alt", sofa.altTxt);
+
+            let productTitle = document.createElement("h3");
+            productArticle.appendChild(productTitle);
+            productTitle.classList.add("productName");
+            productTitle.innerText = sofa.name;
+
+            let productDescription= document.createElement("p");
+            productArticle.appendChild(productDescription);
+            productDescription.classList.add("productDescription");
+            productDescription.innerText = sofa.description ;
+
+          }
+        };
+      })
+      .catch(function(err) {
+          document
+          .getElementById("items")
+          .innerText = "Nous n'avons pas réussi à afficher les articles. Avez-vous bien lancé le serveur local (Port 3000) ?";
+      });
     
+};
+
